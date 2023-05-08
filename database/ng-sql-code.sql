237e68533a14179f1a1365be597e9fb2e0d24a47
Nathan Gartside <gar18014@byui.edu>
2023-05-08 12:44:27 -0600

    Did task one for assignment 2

--- database/ng-sql-code.sql
+++ database/ng-sql-code.sql
@@ -228,3 +228,14 @@ VALUES (
         'White',
         5
     );
+UPDATE inventory
+SET inv_image = REPLACE(
+        inv_image,
+        '/images/',
+        '/images/vehicles/'
+    ),
+    inv_thumbnail = REPLACE(
+        inv_thumbnail,
+        '/images/',
+        '/images/vehicles/'
+    );
