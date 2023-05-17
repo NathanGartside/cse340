3c42f55f3176dcdcf564b081c32b0f9f7f166e6d
Nathan Gartside <gar18014@byui.edu>
2023-05-17 11:54:08 -0600

    Added car classification

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(require("./routes/static"))
 // Index Route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
