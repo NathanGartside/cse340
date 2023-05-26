dd332bdf19f0022d6bd7f0b42fcfea973e6c9935
Nathan Gartside <gar18014@byui.edu>
2023-05-25 18:30:05 -0600

    Did task two of assignment 3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,8 +2,9 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:carId", invController.buildByCarId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/detail/:carId", utilities.handleErrors(invController.buildByCarId));
 
 module.exports = router;
