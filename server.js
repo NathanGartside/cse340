e7b84e56299eb35ffb8b70738a6503938f35ba94
Nathan Gartside <gar18014@byui.edu>
2023-05-30 12:23:30 -0600

    Added sessons and messages

--- server.js
+++ server.js
@@ -6,12 +6,36 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers//baseController")
 const utilities = require("./utilities/")
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
