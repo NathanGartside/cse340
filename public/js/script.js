c3597481a55c67a2acb0a94ed9e2ea195993380f
Nathan Gartside <gar18014@byui.edu>
2023-06-01 17:11:48 -0600

    Added a check for button

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,14 @@
-document.querySelector('.passwordToggle').addEventListener('click', (e) => {
-    let password = document.querySelector('#account_password');
-    let type = password.getAttribute('type');
-    if(type=='password') {
-        password.setAttribute('type', 'text');
-        e.target.innerHTML = "Hide Password";
-    } else {
-        password.setAttribute('type', 'password');
-        e.target.innerHTML = "Show Password";
-    }
-})
+let button = document.querySelector('.passwordToggle');
+if(button) {
+    button.addEventListener('click', (e) => {
+        let password = document.querySelector('#account_password');
+        let type = password.getAttribute('type');
+        if(type=='password') {
+            password.setAttribute('type', 'text');
+            e.target.innerHTML = "Hide Password";
+        } else {
+            password.setAttribute('type', 'password');
+            e.target.innerHTML = "Show Password";
+        }
+    })
+}
