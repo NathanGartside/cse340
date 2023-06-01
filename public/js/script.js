51acaebd9d92019415797db6a1a1504fa97263f7
Nathan Gartside <gar18014@byui.edu>
2023-06-01 16:59:31 -0600

    Added a password toggle

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.passwordToggle').addEventListener('click', (e) => {
+    let password = document.querySelector('#account_password');
+    let type = password.getAttribute('type');
+    if(type=='password') {
+        password.setAttribute('type', 'text');
+        e.target.innerHTML = "Hide Password";
+    } else {
+        password.setAttribute('type', 'password');
+        e.target.innerHTML = "Show Password";
+    }
+})
