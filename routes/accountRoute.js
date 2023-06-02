88da979bf2510e284d056084e4264b5047b64cae
Nathan Gartside <gar18014@byui.edu>
2023-06-02 11:32:12 -0600

    Completated checks

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,15 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 //Route to build account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 //Route to build the registration route
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 //Route to send registration info
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData,utilities.handleErrors(accountController.registerAccount))
+// Process the login attempt
+router.post("/login", (req, res) => { res.status(200).send('login process') })
 
 module.exports = router;
